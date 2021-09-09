26243e399f046d2e4fca055d34bcddb393467b54
Riya-Bisht <manasi.riya2003@gmail.com>
2021-09-09 11:55:32 +0530

    Added RiyaBisht's data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,8 @@
 import { itemType } from "./type/itemType";
 
 import { Niloy } from "./details/Niloy";
+import {RiyaBisht} from "./details/RiyaBisht";
 
-export const data: itemType[] = [Niloy];
+export const data: itemType[] = [Niloy,RiyaBisht];
+
+
