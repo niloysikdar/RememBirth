b122177d04c1d2f50e15fb9dbadf6ef7af94b977
niloysikdar <niloysikdar30@gmail.com>
2021-09-12 21:09:02 +0530

    Updated getDate Function

---
+++ src/utils/getDateAndMonth.ts
@@ -0,0 +1,8 @@
+export const getDate = () => {
+  const date = new Date();
+  const dd = String(date.getDate()).padStart(2, "0");
+  const mm = String(date.getMonth() + 1).padStart(2, "0"); //January is 0!
+  const today = dd + "/" + mm;
+
+  return today;
+};
