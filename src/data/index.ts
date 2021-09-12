3d133addf96c4c23bfbe5f81717bcb3e22db6a31
shreya9811 <shrreeya.jaisswal@gmail.com>
2021-09-12 23:50:10 +0530

    added my data

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import { itemType } from "./type/itemType";
 
 import { Niloy } from "./details/Niloy";
+import {Shreya} from "./details/Shreya";
 
-export const data: itemType[] = [Niloy];
+export const data: itemType[] = [Niloy, Shreya];
