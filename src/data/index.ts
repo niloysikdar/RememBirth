282a37a6d0fce22da6018706f36a6d99187e5ad8
Niti-Shah <emailtonitishah@gmail.com>
2021-09-14 15:15:57 +0530

    Added Niti's data

--- src/data/index.ts
+++ src/data/index.ts
@@ -7,5 +7,6 @@ import {RiyaBisht} from "./details/RiyaBisht";
 import { Hannan } from "./details/Hannan";
 import { harshita } from "./details/harshita";
 import{ Tanushree } from "./details/Tanushree";
+import{ Niti } from "./details/Niti";
 
-export const data: itemType[] = [Niloy , sakshigupta, Medha, RiyaBisht, Hannan, harshita, Tanushree];
+export const data: itemType[] = [Niloy , sakshigupta, Medha, RiyaBisht, Hannan, harshita, Tanushree, Niti];
