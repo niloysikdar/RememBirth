27e25ce7a828a047f9eee06cd65f47f222654ec2
sarikaji22 <sarika.vsa01@gmail.com>
2021-09-16 21:59:02 +0530

    Added Sarika's Data

---
+++ src/data/details/sarika.ts
@@ -0,0 +1,10 @@
+import { itemType } from "../type/itemType";
+
+export const sarika: itemType = {
+  name: "Sarika Kushwaha", // Add your fullname here
+  birthDate: "22/12", // Add your birthDate in the format of DD/MM
+  imageURL: "https://imgur.com/gna082V.jpg", // Go to "https://imgur.com/upload", upload your image and paste the link here
+  gitHubURL: "https://github.com/sarikaji22", // Paste your GitHub profile URL here
+  linkedInURL: "https://www.linkedin.com/in/sarika-kushwaha-186a881a0", // Paste your LinkedIn profile URL here
+  twitterURL: "", //Paste your Twitter profile URL here (optional)
+};
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,5 +1,6 @@
 import { itemType } from "./type/itemType";
 
 import { Niloy } from "./details/Niloy";
+import{ sarika } from "./details/sarika";
 
-export const data: itemType[] = [Niloy];
+export const data: itemType[] = [Niloy, sarika];
