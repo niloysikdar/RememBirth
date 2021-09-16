4bf0e6d25ddc12eb88ff8c15c88496a6e02ccc21
niloysikdar <niloysikdar30@gmail.com>
2021-09-16 22:57:16 +0530

    Added Husky,Lint and Prettier

--- src/data/index.ts
+++ src/data/index.ts
@@ -1,13 +1,13 @@
-import { itemType } from "./type/itemType";
+import { itemType } from './type/itemType';
 
-import { Niloy } from "./details/Niloy";
-import { sakshigupta } from "./details/sakshigupta";
-import { Medha } from "./details/Medha";
-import { RiyaBisht } from "./details/RiyaBisht";
-import { Hannan } from "./details/Hannan";
-import { harshita } from "./details/harshita";
-import { Tanushree } from "./details/Tanushree";
-import { Shreya } from "./details/Shreya";
+import { Niloy } from './details/Niloy';
+import { sakshigupta } from './details/sakshigupta';
+import { Medha } from './details/Medha';
+import { RiyaBisht } from './details/RiyaBisht';
+import { Hannan } from './details/Hannan';
+import { harshita } from './details/harshita';
+import { Tanushree } from './details/Tanushree';
+import { Shreya } from './details/Shreya';
 
 export const data: itemType[] = [
   Niloy,
