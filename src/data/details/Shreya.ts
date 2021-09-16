72cff3fbf85e2b357fe94f8137cd449449ef650f
niloysikdar <niloysikdar30@gmail.com>
2021-09-16 21:35:56 +0530

    Data Fixes

--- src/data/details/Shreya.ts
+++ src/data/details/Shreya.ts
@@ -3,8 +3,8 @@ import { itemType } from "../type/itemType";
 export const Shreya: itemType = {
   name: "Shreya Jaiswal", // Add your fullname here
   birthDate: "27/11", // Add your birthDate in the format of DD/MM
-  imageURL: "https://i.imgur.com/5PEOIPW.jpg", // Go to "https://imgur.com/upload", upload your image and paste the link here
+  imageURL: "https://i.imgur.com/YklGH1F.png", // Go to "https://imgur.com/upload", upload your image and paste the link here
   gitHubURL: "https://github.com/shreya9811", // Paste your GitHub profile URL here
-  linkedInURL: "linkedin.com/in/shreya-jaiswal-84919b160", // Paste your LinkedIn profile URL here
+  linkedInURL: "https://linkedin.com/in/shreya-jaiswal-84919b160", // Paste your LinkedIn profile URL here
   twitterURL: "", //Paste your Twitter profile URL here (optional)
 };
