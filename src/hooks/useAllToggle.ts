6890484a1a44aee04a8a256d09e94a645bd433a1
niloysikdar <niloysikdar30@gmail.com>
2021-09-16 21:01:18 +0530

    Added useAllToggle hook

---
+++ src/hooks/useAllToggle.ts
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+const useAllToggle = () => {
+  const [isAll, setIsAll] = useState(true);
+
+  const setAll = () => setIsAll(true);
+  const setToday = () => setIsAll(false);
+
+  return { isAll, setAll, setToday };
+};
+
+export { useAllToggle };
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,18 @@
-import { useState } from "react";
 import { AnimatePresence } from "framer-motion";
+
+import { useAllToggle } from "../../hooks/useAllToggle";
+import { useModal } from "../../hooks/useModal";
 
 import { Header } from "../../components/Header";
 import { Card } from "../../components/Card";
 import { data } from "../../data";
 import { About } from "../../components/About";
-import { useModal } from "../../hooks/useModal";
 
 import { getDate } from "../../utils/getDateAndMonth";
 import styles from "./style.module.scss";
 
 const Home = () => {
-  const [isAll, setIsAll] = useState(true);
-  const setAll = () => setIsAll(true);
-  const setToday = () => setIsAll(false);
-
+  const { isAll, setAll, setToday } = useAllToggle();
   const { isModalOpen, open, close } = useModal();
 
   return (
