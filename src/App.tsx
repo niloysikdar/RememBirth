a95e00fd4214a5ed477c03b137b73b4fa51b48df
Niloy Sikdar <niloysikdar30@gmail.com>
2021-10-16 14:57:06 +0530

    Added Routing and 404 Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import { Home } from './pages/Home';
+import { Page404 } from './pages/404';
 
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Home} />
+        <Route component={Page404} />
+      </Switch>
+    </Router>
+  );
 };
 
 export { App };
