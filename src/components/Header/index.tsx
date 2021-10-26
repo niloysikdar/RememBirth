9c0951998613de585d97b913e52225f0b4152291
Jay Clark <jay@jayeclark.dev>
2021-10-26 12:50:48 -0400

    Make navbar responsive
    Signed-off-by: Jay Clark <jay@jayeclark.dev>

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ interface headerProps {
 const Header: FC<headerProps> = (props) => {
   return (
     <div className={styles.header}>
-      <h3>RememBirth</h3>
+      <h3 className={styles.title}>RememBirth</h3>
       <div className={styles.buttons}>
         <button className={styles.toggleButton} onClick={props.setAll}>
           All
@@ -22,15 +22,15 @@ const Header: FC<headerProps> = (props) => {
         <button className={styles.toggleButton} onClick={props.setToday}>
           Today
         </button>
+        <motion.button
+          whileHover={{ scale: 1.1 }}
+          whileTap={{ scale: 0.9 }}
+          className={styles.info}
+          onClick={props.aboutOpen}
+        >
+          <FaInfo size={25} />
+        </motion.button>
       </div>
-      <motion.button
-        whileHover={{ scale: 1.1 }}
-        whileTap={{ scale: 0.9 }}
-        className={styles.info}
-        onClick={props.aboutOpen}
-      >
-        <FaInfo size={25} />
-      </motion.button>
     </div>
   );
 };
